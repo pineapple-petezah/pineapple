4dda556881c54cef6dca650ae425e2d700b99155
Brunys <brunys@brunysixl.xyz>
2024-12-19 18:21:07 -0500

    full beta testing

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/ixl/',
-    bare:'https://brunysixlworkisgoatedfrfronbandongokszvycentralv2outnow.v2202412246404304352.megasrv.de/bare/', 
+    bare:'https://v2202412246404304352.megasrv.de/bare/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
